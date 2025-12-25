4b730e892202cfabb0fe78bda29affbe8b67ae89
DrobyshevIvan <vanyadrob0@gmail.com>
2025-12-25 17:34:23 +0200

    Changed

--- GateSense/GateSense.Infrastructure/Common/PagedList/PagedList.cs
+++ GateSense/GateSense.Infrastructure/Common/PagedList/PagedList.cs
@@ -26,7 +26,7 @@ public class PagedList<T> : IPagedList<T>
     public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
     {
         var count = await source.CountAsync();
-        var items = await source.Skip(pageNumber * pageSize).Take(pageSize).ToListAsync();
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
         return new PagedList<T>(items, count, pageNumber, pageSize);
     }
 }
